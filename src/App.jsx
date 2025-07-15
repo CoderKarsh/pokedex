c275b8e646b662ffaa82ebbf6f6cf8596d1d0109
CoderKarsh <shreeut2704@gmail.com>
2025-07-16 01:29:59 +0530

    Add filters and improve design

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import Card from "./components/Card";
 
 function App() {
   const [allPokemonData, setAllPokemonData] = useState([]);
-  const offset = 40;
-  const limit = 20;
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(20);
+  const [searchTerm, setSearchTerm] = useState("");
+
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`)
       .then((res) => res.json())
@@ -18,21 +20,96 @@ function App() {
           setAllPokemonData(pokemonDataArray)
         );
       });
-  }, []);
+  }, [limit, offset]);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${1302}&offset=${0}`)
+      .then((res) => res.json())
+      .then((fetchedData) => {
+        const fetchPromises = fetchedData.results.map((result) =>
+          fetch(result.url).then((res) => res.json())
+        );
+
+        Promise.all(fetchPromises).then((pokemonDataArray) =>
+          setAllPokemonData(
+            pokemonDataArray.filter((data) =>
+              data.name.includes(searchTerm.toLowerCase())
+            )
+          )
+        );
+      });
+  }, [searchTerm]);
+
+  console.log();
 
   function handleHover() {}
 
   console.log(allPokemonData);
+  function handleSubmit(formData) {
+    const formOffset = formData.get("offset-input");
+    const formLimit = formData.get("limit-input");
+    const formName = formData.get("name-input");
+
+    formName !== "" ? setSearchTerm(formName) : null;
+
+    formOffset !== ""
+      ? formOffset > 0
+        ? setOffset(formOffset - 1)
+        : alert("Offset should be 1 or more")
+      : null;
+    formLimit
+      ? formLimit > 0
+        ? setLimit(formLimit)
+        : alert("No. of Cards should be 1 or more")
+      : null;
+  }
   return (
-    <div className="card-container">
-      {allPokemonData.map((pokemonData) => (
-        <Card
-          key={pokemonData.id}
-          pokemonObject={pokemonData}
-          handleHover={handleHover()}
-        />
-      ))}
-    </div>
+    <>
+      <form action={handleSubmit}>
+        <div className="input-label-container">
+          <label htmlFor="name-input">Offset: </label>
+          <input
+            type="text"
+            name="name-input"
+            id="name-input"
+            // defaultValue={1}
+            placeholder="Charizard"
+          />
+        </div>
+
+        <div className="input-label-container">
+          <label htmlFor="offset-input">Offset: </label>
+          <input
+            type="number"
+            name="offset-input"
+            id="offset-input"
+            // defaultValue={1}
+            placeholder="1"
+          />
+        </div>
+        <div className="input-label-container">
+          <label htmlFor="limit-input">No. of Cards: </label>
+          <input
+            type="number"
+            name="limit-input"
+            id="limit-input"
+            // defaultValue={20}
+            placeholder="20"
+          />
+        </div>
+
+        <button type="submit">Filter Pokemon</button>
+      </form>
+      <div className="card-container">
+        {allPokemonData.map((pokemonData) => (
+          <Card
+            key={pokemonData.id}
+            pokemonObject={pokemonData}
+            handleHover={handleHover()}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,27 @@
 import "./Card.css";
 import { typeColors } from "../typeColors";
 export default function Card({ pokemonObject }) {
+  function truncateText(str) {
+    return str.length > 9 ? str.substring(0, 8) + "..." : str;
+  }
+
   return (
     <div
       className="card"
-      style={{ backgroundColor: typeColors[pokemonObject.types[0].type.name] }}
+      style={{
+        backgroundColor: typeColors[`${pokemonObject.types[0].type.name}Bg`],
+      }}
     >
+      <div className="shine"></div>
       <img
         src={`${pokemonObject.sprites.front_default}`}
         alt={`Image of ${pokemonObject.name}`}
       />
-      <h2>
-        {pokemonObject.name.charAt(0).toUpperCase() +
-          pokemonObject.name.slice(1)}
+      <h2 style={{ overflow: "hidden", textOverflow: "ellipsis" }}>
+        {truncateText(
+          pokemonObject.name.charAt(0).toUpperCase() +
+            pokemonObject.name.slice(1)
+        )}
       </h2>
       {pokemonObject.types.map((type, index) => (
         <span
