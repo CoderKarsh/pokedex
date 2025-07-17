97dc9347025741ea1aa7d93e5900dcd5a1f3c552
CoderKarsh <shreeut2704@gmail.com>
2025-07-17 08:30:52 +0530

    Add separate component for Search Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import Card from "./components/Card";
+import SearchBar from "./components/SearchBar.jsx";
 import { initialData } from "../initialData.js";
 
 function App() {
@@ -9,16 +10,6 @@ function App() {
   const [offset, setOffset] = useState(1);
   const [limit, setLimit] = useState(20);
   const [searchTerm, setSearchTerm] = useState("");
-  const [searchInput, setSearchInput] = useState("");
-
-  // Debounce search input
-  useEffect(() => {
-    const debounceTimer = setTimeout(() => {
-      setSearchTerm(searchInput);
-    }, 300); // 300ms debounce time
-
-    return () => clearTimeout(debounceTimer);
-  }, [searchInput]);
 
   // Loading Data
   useEffect(() => {
@@ -48,69 +39,15 @@ function App() {
     }
   }, [allPokemonData, offset, limit, searchTerm]);
 
-  function handleSubmit(formData) {
-    const formOffset = parseInt(formData.get("offset-input"), 10);
-    const formLimit = parseInt(formData.get("limit-input"), 10);
-    const formName = formData.get("name-input");
-
-    if (formName !== null && formName.trim() !== "") {
-      setSearchTerm(formName);
-      setLimit(1302);
-      setOffset(1);
-    } else {
-      setSearchTerm("");
-      setLimit(isNaN(formLimit) ? 20 : formLimit);
-      setOffset(isNaN(formOffset) ? 1 : formOffset);
-    }
-
-    if (formOffset <= 0) {
-      alert("Offset should be 1 or more");
-    }
-    if (formLimit <= 0) {
-      alert("No. of Cards should be 1 or more");
-    }
-  }
-
   console.log(currentPokemonData);
   return (
     <>
       <h1>PokéDex</h1>
-      <div className="input-container">
-        <div className="input-label-container">
-          <label htmlFor="name-input">Name: </label>
-          <input
-            type="text"
-            name="name-input"
-            id="name-input"
-            placeholder="Charizard"
-            onChange={(e) => setSearchInput(e.target.value)}
-          />
-        </div>
-        <form action={handleSubmit}>
-          <div className="input-label-container">
-            <label htmlFor="offset-input">Offset: </label>
-            <input
-              type="number"
-              name="offset-input"
-              id="offset-input"
-              // defaultValue={1}
-              placeholder="1"
-            />
-          </div>
-          <div className="input-label-container">
-            <label htmlFor="limit-input">Cards: </label>
-            <input
-              type="number"
-              name="limit-input"
-              id="limit-input"
-              // defaultValue={20}
-              placeholder="20"
-            />
-          </div>
-
-          <button type="submit">Filter Pokemon</button>
-        </form>
-      </div>
+      <SearchBar
+        setLimit={setLimit}
+        setOffset={setOffset}
+        setSearchTerm={setSearchTerm}
+      />
       <div className="card-container">
         {currentPokemonData.map((pokemonData) => (
           <Card key={pokemonData.id} pokemonObject={pokemonData} />
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+
+export default function SearchBar({ setSearchTerm, setLimit, setOffset }) {
+  const [searchInput, setSearchInput] = useState("");
+
+  // Debounce
+  useEffect(() => {
+    const debounceTimer = setTimeout(() => {
+      setSearchTerm(searchInput);
+    }, 300); // 300ms debounce time
+
+    return () => clearTimeout(debounceTimer);
+  }, [searchInput, setSearchTerm]);
+
+  function handleSubmit(formData) {
+    const formOffset = parseInt(formData.get("offset-input"), 10);
+    const formLimit = parseInt(formData.get("limit-input"), 10);
+    const formName = formData.get("name-input");
+
+    if (formName !== null && formName.trim() !== "") {
+      setSearchTerm(formName);
+      setLimit(20);
+      setOffset(1);
+    } else {
+      setSearchTerm("");
+      setLimit(isNaN(formLimit) ? 1302 : formLimit);
+      setOffset(isNaN(formOffset) ? 1 : formOffset);
+    }
+
+    if (formOffset <= 0) {
+      alert("Offset should be 1 or more");
+    }
+    if (formLimit <= 0) {
+      alert("No. of Cards should be 1 or more");
+    }
+  }
+
+  return (
+    <>
+      <div className="input-container">
+        <div className="input-label-container">
+          <label htmlFor="name-input">Name: </label>
+          <input
+            type="text"
+            name="name-input"
+            id="name-input"
+            placeholder="e.g., Charizard"
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+        </div>
+        <form action={handleSubmit}>
+          <div className="input-label-container">
+            <label htmlFor="offset-input">Offset: </label>
+            <input
+              type="number"
+              name="offset-input"
+              id="offset-input"
+              // defaultValue={1}
+              placeholder="e.g., 1"
+            />
+          </div>
+          <div className="input-label-container">
+            <label htmlFor="limit-input">Cards: </label>
+            <input
+              type="number"
+              name="limit-input"
+              id="limit-input"
+              // defaultValue={20}
+              placeholder="e.g., 20"
+            />
+          </div>
+
+          <button type="submit">Filter Pokemon</button>
+        </form>
+      </div>
+    </>
+  );
+}
