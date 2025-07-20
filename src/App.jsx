4739d720a802708ac200806681f8f9b97f0a81ef
CoderKarsh <shreeut2704@gmail.com>
2025-07-20 12:11:16 +0530

    remove extranneous heading

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Main from "./components/Main.jsx";
 function App() {
   return (
     <>
-      <h1>PokéDex</h1>
       <PokemonDataFetcher>
         {(data) => <Main allPokemonData={data} />}
       </PokemonDataFetcher>
