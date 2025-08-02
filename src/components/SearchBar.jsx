40a924c4190046773d02ede66f672a5907766070
CoderKarsh <shreeut2704@gmail.com>
2025-08-02 20:48:34 +0530

    Change icon location and add shadow

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { PiMagnifyingGlassBold } from "react-icons/pi";
 import { IconContext } from "react-icons";
 import "../styles/SearchBar.css";
 
 export default function SearchBar({ setSearchTerm }) {
   const [searchInput, setSearchInput] = useState("");
+  const searchBarRef = useRef(null);
+  const [isSticky, setIsSticky] = useState(false);
 
   // Debounce
   useEffect(() => {
@@ -15,11 +17,39 @@ export default function SearchBar({ setSearchTerm }) {
     return () => clearTimeout(debounceTimer);
   }, [searchInput, setSearchTerm]);
 
+  // Intersection Observer to detect when the search bar is sticky
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // `isIntersecting` is false when the element is no longer visible
+        // in the viewport, which is when it becomes sticky.
+        setIsSticky(!entry.isIntersecting);
+      },
+      {
+        root: null, // Use the viewport as the root
+        rootMargin: "0px 0px -100% 0px", // A small margin can help prevent flickering
+        threshold: 0, // Trigger when any part of the element is visible
+      }
+    );
+
+    if (searchBarRef.current) {
+      observer.observe(searchBarRef.current);
+    }
+
+    return () => {
+      if (searchBarRef.current) {
+        observer.unobserve(searchBarRef.current);
+      }
+    };
+  }, []);
   return (
     <>
-      <div className="input-container">
+      <div
+        className={`input-container  ${!isSticky ? "sticky-shadow" : ""}`}
+        ref={searchBarRef}
+      >
         <div className="input-label-container">
-          <IconContext.Provider value={{ className: "search-icon" }}>
+          <IconContext.Provider value={{ className: `search-icon` }}>
             <PiMagnifyingGlassBold />
           </IconContext.Provider>
           {/* <label htmlFor="name-input">Name: </label> */}
